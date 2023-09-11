b6fa651725c10678354525c1c708f8959a127e28
Eric McKenzie <46291001@edu.cbe.ab.ca>
2023-09-11 10:22:16 -0600

    linear search is working but only returns the term searched for rather than index

--- linear-search.js
+++ linear-search.js
@@ -1,12 +1,36 @@
+
 function linearSearch(array, searchTerm) {
-	/*
+	let answer = searchTerm
+	array.forEach(element => {
+		if (element = answer) {
+			//let answer = array.indexOf(element)
+			console.log (element)
+			
+			
+		}
+
+	
+		
+	});
+	return answer;
+
+}
+
+
+
+
+	
+
+
+
+
+
+
+module.exports = linearSearch;
+	
+/*
 	 ** Check each element from start to finish
 	 ** until you find an element that matches the search term
 	 **
 	 ** Return the index of the found element
 	 */
-
-	return null;
-}
-
-module.exports = linearSearch;
