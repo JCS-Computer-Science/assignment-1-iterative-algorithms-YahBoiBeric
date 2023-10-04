b80f1377e2ccc7b00f99c021ef266fac6907b2a7
Eric McKenzie <46291001@edu.cbe.ab.ca>
2023-10-04 09:50:42 -0600

    Finished!!!

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,6 +1,22 @@
 function bubbleSort(array) {
+               loop = array.length
+for (let i = 0; i < loop -1; i++) {
+    swapped = false
     
-	
+    for (let j = 0; j < loop - 1; j++) {
+        if (array[j]>array[j+1]) {
+            var b = array[j]
+            array[j] = array[j+1]
+            array[j+1] = b
+            swapped =true
+        }
+        
+    }
+    if (swapped=false) {
+        break
+        
+    }
+}
 
 	return array;
 }
@@ -13,5 +29,6 @@ function bubbleSort(array) {
     ** Repeat the above process until you get through an entire
     ** cycle without needing any swaps
     */
+   //https://www.tutorialspoint.com/data_structures_algorithms/bubble_sort_algorithm.htm
 
 module.exports = bubbleSort;
--- insertion-sort.js
+++ insertion-sort.js
@@ -1,29 +1,6 @@
 function insertionSort(array) {
 	
-	/*for (let i = 0; i < array.length; i++) {
-		let ind = i
-		let subject = array[i]
-		 for (let j = 0; j < array.length +1; j++) {
-			subject = array[j + 1]
-				ind = [j]
-			if (array[j] > subject) {
-				
-			let temp = subject
-			array[ind] = array[i]
-			array[i] = temp
-
-				
-			}
-			
-		 }
-
-
-		
-		
-	 }
-
-
-	*/
+	
 	let l = array.length
 	for (let i = 1; i < l; i++) {
 	let curVal = array[i]
@@ -35,7 +12,7 @@ function insertionSort(array) {
 	      array[n+1]= curVal
 	}
 	return array;
-}
+}   
 	/*
 	 ** For each element in the array, swap it with the element
 	 ** before it over and over as long as the element before it
